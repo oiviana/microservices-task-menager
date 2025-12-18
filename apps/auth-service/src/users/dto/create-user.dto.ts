d7128e60463e8f5327ca06f31beb223855b35210
oiviana <vianalucas80@gmail.com>
2025-12-18 15:41:42 -0300

    feat:(auth-service) add user dto

--- apps/auth-service/src/users/dto/create-user.dto.ts
+++ apps/auth-service/src/users/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+import { IsString, IsEmail, MinLength } from 'class-validator';
+export class CreateUserDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6, { message: 'A senha deve ter pelo menos 6 caracteres' })
+  password: string; 
+}
