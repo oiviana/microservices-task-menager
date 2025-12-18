34815dd43e9706c5efed825565fdecc91bafe8fc
oiviana <vianalucas80@gmail.com>
2025-12-18 15:05:23 -0300

    refactor:(auth-service) use main.ts for microservices and DTO

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [process.env.RABBITMQ_URL || 'amqp://localhost:5672'],
+        queue: 'auth_queue',
+        queueOptions: {
+          durable: true,
+        },
+      },
+    },
+  );
+
+  // ValidationPipe para DTO
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen();
 }
 bootstrap();
