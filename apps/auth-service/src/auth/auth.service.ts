eb8e38af5ea17b11a05f668523c7384d3f981f14
oiviana <vianalucas80@gmail.com>
2025-12-19 20:40:36 -0300

    feat:(auth-service) add auth services

---
+++ apps/auth-service/src/auth/auth.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async register(data: { name: string; email: string; password: string }) {
+    const user = await this.usersService.create(data);
+
+    return {
+      user,
+      ...this.generateTokens(user.id, user.email),
+    };
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.usersService.findByEmail(email);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const passwordValid = await bcrypt.compare(password, user.passwordHash);
+
+    if (!passwordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    return {
+      user,
+      ...this.generateTokens(user.id, user.email),
+    };
+  }
+
+  private generateTokens(userId: string, email: string) {
+    const payload = { sub: userId, email };
+
+    const accessToken = this.jwtService.sign(payload, {
+      expiresIn: '15m',
+    });
+
+    const refreshToken = this.jwtService.sign(payload, {
+      expiresIn: '7d',
+    });
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+}
