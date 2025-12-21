e49d9b001744c84c78930a519014578961cb600d
oiviana <vianalucas80@gmail.com>
2025-12-21 17:17:23 -0300

    feat:(api-gateway) add prefix and cors

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  
   // ValidationPipe para DTO
   app.useGlobalPipes(
     new ValidationPipe({
