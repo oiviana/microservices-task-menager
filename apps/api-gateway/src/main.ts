731e5fedfc6126bff50b4c33615ffe3bd03d920c
oiviana <vianalucas80@gmail.com>
2025-12-21 18:57:06 -0300

    feat:(api-gateway) add swagger config

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,7 +9,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
   app.enableCors();
-  
+
   // ValidationPipe para DTO
   app.useGlobalPipes(
     new ValidationPipe({
@@ -17,5 +18,17 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // Configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Gateway')
+    .setDescription('Microservices Gateway')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
 }
 bootstrap();
