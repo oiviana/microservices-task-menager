0aaf46372b316f94d632849b235a31a7e9b48c7f
oiviana <vianalucas80@gmail.com>
2025-12-22 16:26:07 -0300

    feat:(api-gateway) jwt provider

--- apps/api-gateway/src/auth/auth.module.ts
+++ apps/api-gateway/src/auth/auth.module.ts
@@ -1,15 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from '@/strategies/jwt.strategy';
 
 @Module({
   imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'dev_secret',
+      signOptions: { expiresIn: '1h' },
+    }),
     ClientsModule.register([
       {
         name: 'AUTH_SERVICE',
         transport: Transport.RMQ,
         options: {
-          urls: [process.env.RABBITMQ_URL?? 'amqp://admin:admin@rabbitmq:5672'],
+          urls: [process.env.RABBITMQ_URL ?? 'amqp://admin:admin@rabbitmq:5672'],
           queue: 'auth_queue',
           queueOptions: { durable: true },
         },
@@ -17,5 +25,6 @@ import { AuthController } from './auth.controller';
     ]),
   ],
   controllers: [AuthController],
+  providers: [JwtStrategy],
 })
 export class AuthModule {}
