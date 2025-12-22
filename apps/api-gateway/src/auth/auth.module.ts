18b1007848f80b9516ba61a05184b515462b87c6
oiviana <vianalucas80@gmail.com>
2025-12-22 00:14:17 -0300

    feat:(api-gateway) connecting with auth-service

--- apps/api-gateway/src/auth/auth.module.ts
+++ apps/api-gateway/src/auth/auth.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AuthController } from './auth.controller';
 
 @Module({
-  controllers: [AuthController]
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'AUTH_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL?? 'amqp://admin:admin@rabbitmq:5672'],
+          queue: 'auth_queue',
+          queueOptions: { durable: true },
+        },
+      },
+    ]),
+  ],
+  controllers: [AuthController],
 })
 export class AuthModule {}
