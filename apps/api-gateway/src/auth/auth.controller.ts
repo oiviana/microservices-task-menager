dd9f78ef62b857aa15f795b798c3f4bdb52474e5
oiviana <vianalucas80@gmail.com>
2025-12-22 00:15:36 -0300

    feat:(api-gateway) auth controller for auth service

--- apps/api-gateway/src/auth/auth.controller.ts
+++ apps/api-gateway/src/auth/auth.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { JwtAuthGuard } from "@/guards/jwt-auth.guard";
+import { JwtRefreshGuard } from "@/guards/jwt-refresh.guard";
+import { Body, Controller, Inject, Post, Req, UseGuards } from "@nestjs/common";
+import { ClientProxy } from "@nestjs/microservices";
+import { RegisterDto } from "./dto/register.dto";
+import { LoginDto } from "./dto/login.dto";
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    @Inject('AUTH_SERVICE') private readonly authClient: ClientProxy,
+  ) {}
+
+  @Post('register')
+  register(@Body() dto: RegisterDto) {
+    return this.authClient.send('auth.register', dto);
+  }
+
+  @Post('login')
+  login(@Body() dto: LoginDto) {
+    return this.authClient.send('auth.login', dto);
+  }
+
+  @UseGuards(JwtRefreshGuard)
+  @Post('refresh')
+  refresh(@Req() req, @Body() body) {
+    return this.authClient.send('auth.refresh', {
+      userId: req.user.id,
+      email: req.user.email,
+      refreshToken: body.refreshToken,
+    });
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('logout')
+  logout(@Req() req) {
+    return this.authClient.send('auth.logout', {
+      userId: req.user.id,
+    });
+  }
+}
