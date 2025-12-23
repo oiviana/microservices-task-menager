7a214e8703e735116552b50cff74c9ba848021a2
oiviana <vianalucas80@gmail.com>
2025-12-23 13:38:00 -0300

    feat:(api-gateway) new modules

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '@/auth/auth.module';
+import { TasksModule } from '@/tasks/tasks.module';
+import { CommentsModule } from '@/comments/comments.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, TasksModule, CommentsModule],
 })
 
 export class AppModule {}
