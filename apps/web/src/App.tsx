8fa5d24ce893c2b59709a5e72fa4578c80855654
oiviana <vianalucas80@gmail.com>
2025-12-24 15:25:34 -0300

    fix:(web) toaster calling

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,11 +1,13 @@
 import { AppProviders } from './providers/AppProviders'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from '@/router'
+import { Toaster } from 'sonner'
 
 export function App() {
   return (
     <AppProviders>
       <RouterProvider router={router} />
+      <Toaster position="top-right" richColors />
     </AppProviders>
   )
 }
