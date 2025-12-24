df439d796fdc3b58afaba8e602badf968603ee63
oiviana <vianalucas80@gmail.com>
2025-12-24 15:18:44 -0300

    fix:(api-gateway) cors for localhost

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -7,7 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-  app.enableCors();
+
+  app.enableCors({
+    origin: 'http://localhost:3000', // front rodando fora do docker
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
