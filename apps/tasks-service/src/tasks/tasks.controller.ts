707442ce2c3810c40c4a1950db3f3db99a99ff5b
oiviana <vianalucas80@gmail.com>
2025-12-28 20:25:30 -0300

    chore:(tasks-services) tasks logger

--- apps/tasks-service/src/tasks/tasks.controller.ts
+++ apps/tasks-service/src/tasks/tasks.controller.ts
@@ -5,13 +5,22 @@ import { CreateTaskMessageDto } from './dto/create-task-message.dto';
 import { UpdateTaskMessageDto } from './dto/update-task-message.dto';
 import { FindAllTasksMessageDto } from './dto/find-all-tasks-message.dto';
 import { FindOneTaskMessageDto } from './dto/find-one-task-message.dto';
+import { CustomLogger } from '@repo/logger';
 
 @Controller()
 export class TasksController {
-  constructor(private readonly tasksService: TasksService) {}
+  private readonly context = TasksController.name;
+
+  constructor(
+    private readonly tasksService: TasksService,
+    private readonly logger: CustomLogger,
+  ) {}
 
   @MessagePattern('task.create')
   create(@Payload() payload: CreateTaskMessageDto) {
+    this.logger.log(`Evento recebido: task.create`, this.context);
+    this.logger.log(`Payload: ${JSON.stringify(payload)}`, this.context);
+
     return this.tasksService.create({
       ...payload,
       dueDate: payload.dueDate ? new Date(payload.dueDate) : undefined,
@@ -20,6 +29,9 @@ export class TasksController {
 
   @MessagePattern('task.findAll')
   findAll(@Payload() payload?: FindAllTasksMessageDto) {
+    this.logger.log(`Evento recebido: task.findAll`, this.context);
+    this.logger.log(`Payload: ${JSON.stringify(payload)}`, this.context);
+
     return this.tasksService.findAll(
       payload?.page ?? 1,
       payload?.size ?? 10,
@@ -28,11 +40,17 @@ export class TasksController {
 
   @MessagePattern('task.findOne')
   findOne(@Payload() payload: FindOneTaskMessageDto) {
+    this.logger.log(`Evento recebido: task.findOne`, this.context);
+    this.logger.log(`Payload: ${JSON.stringify(payload)}`, this.context);
+
     return this.tasksService.findOne(payload.id);
   }
 
   @MessagePattern('task.update')
   update(@Payload() payload: UpdateTaskMessageDto) {
+    this.logger.log(`Evento recebido: task.update`, this.context);
+    this.logger.log(`Payload: ${JSON.stringify(payload)}`, this.context);
+
     return this.tasksService.update(payload.id, {
       ...payload.data,
       dueDate: payload.data?.dueDate
@@ -43,6 +61,9 @@ export class TasksController {
 
   @MessagePattern('task.remove')
   remove(@Payload() payload: FindOneTaskMessageDto) {
+    this.logger.log(`Evento recebido: task.remove`, this.context);
+    this.logger.log(`Payload: ${JSON.stringify(payload)}`, this.context);
+
     return this.tasksService.remove(payload.id);
   }
 }
